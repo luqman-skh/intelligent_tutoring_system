c43d7bcd267a1346487667af4064313484ebd72c
Luqman Shaikh <58136409+luqman-skh@users.noreply.github.com>
2025-07-06 15:37:43 +0200

    Update FirebaseDB.js

--- react-quiz-app/src/components/Firebase/FirebaseDB.js
+++ react-quiz-app/src/components/Firebase/FirebaseDB.js
@@ -9,13 +9,13 @@ import {
   } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAT2KHPIlH2AUZvtiiU1AZYMpAXXViFuak",
+  apiKey: "A",
   authDomain: "its-hcat.firebaseapp.com",
   projectId: "its-hcat",
   storageBucket: "its-hcat.firebasestorage.app",
-  messagingSenderId: "172113702731",
-  appId: "1:172113702731:web:79631e07c871e07165c2ce",
-  measurementId: "G-LNC6MNX9ZR"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
