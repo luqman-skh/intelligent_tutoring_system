a43106c6f393d158916bdf8f2e08371a2ea1c9c3
Luqman Shaikh <58136409+luqman-skh@users.noreply.github.com>
2025-07-06 15:34:27 +0200

    Update firebaseConfig.js

--- react-quiz-app/src/firebaseConfig.js
+++ react-quiz-app/src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAT2KHPIlH2AUZvtiiU1AZYMpAXXViFuak",
+    apiKey: "",
     authDomain: "its-hcat.firebaseapp.com",
     projectId: "its-hcat",
     storageBucket: "its-hcat.firebasestorage.app",
-    messagingSenderId: "172113702731",
-    appId: "1:172113702731:web:79631e07c871e07165c2ce",
-    measurementId: "G-LNC6MNX9ZR"
+    messagingSenderId: "",
+    appId: "e",
+    measurementId: ""
   };
   
 const app = initializeApp(firebaseConfig);
